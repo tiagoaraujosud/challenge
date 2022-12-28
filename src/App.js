66dd653dacb019c064388e1b2d5ba6cf0aeefea4
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-12-28 19:27:32 -0300

    Get X and Y coordinates using event, clientX, clientY and useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        teste
 
-      </header>
+function App() {
+
+  const [list, setList] = useState([]);
+
+  const handleClick = (event) => {
+    
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+    //setList();
+    console.log(newDot)
+  }
+
+  return (
+    
+    <div id='page' onClick={handleClick}>
+      <span className='dot'></span>
     </div>
+
   );
 }
 
