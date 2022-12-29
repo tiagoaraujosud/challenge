8cfdcf0caf8340ddc643678bac21481d59aa3f10
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-12-29 20:27:49 -0300

    Create redo function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
   const [list, setList] = useState([]);
+  const [undid, setUndid] = useState([]);
 
   const handleClick = (event) => {
     
@@ -16,8 +17,16 @@ function App() {
     setList((prev) => [...prev, newDot]);
     console.log(newDot)
   }
+
   const handleUndo = (event) => {
     event.stopPropagation();
+
+    if (list.length === 0) {
+      return;
+    }
+
+    const lastItem = list[list.length - 1];
+    setUndid((prev) => [...prev, lastItem]);
 
     setList((prev) => {
       const newArray = [...prev].slice(0, -1);
@@ -25,10 +34,24 @@ function App() {
     })
   }
 
+  const handleRedo = (event) => {
+    event.stopPropagation();
+    
+    const recoveredDot = undid[undid.length - 1];
+
+    setUndid((prev) => {
+      const newArray = [...prev].slice(0, -1);
+      return newArray;
+    })
+
+    setList((prev) => [...prev, recoveredDot]);
+  }
+
   return (
     
     <div id='page' onClick={handleClick}>
       <button onClick={handleUndo}>Undo</button>
+      <button onClick={handleRedo}>Redo</button>
       {list.map((item) => (
         <span className='dot' style={{left: item.clientX, top: item.clientY}}></span>
       ))}
